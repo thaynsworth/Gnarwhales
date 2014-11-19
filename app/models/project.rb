315fad3a4c5af901ad8364ac613dbd867b71d54e
JamesCPLiu <jamesc.pliu@gmail.com>
2014-11-19 15:58:57 -0500

    added user_id to proj validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, :description, :start_date, :end_date, presence: true
+  validates :title, :description, :start_date, :end_date, :user_id, presence: true
 end
