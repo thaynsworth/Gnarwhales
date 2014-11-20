6af413d4f1f5e9ef8baacc84cdbc4972348695a3
PalermoD <palermo.deschamps@gmail.com>
2014-11-19 23:02:48 -0500

    edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119194713) do
+ActiveRecord::Schema.define(version: 20141120010348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20141119194713) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "summary"
+    t.string   "string"
+    t.string   "website"
+    t.string   "location"
   end
 
 end
