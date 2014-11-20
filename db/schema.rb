dc51bdd1a3323c461e79aefae0bda61e177a54a6
PalermoD <palermo.deschamps@gmail.com>
2014-11-20 16:57:01 -0500

    home and user page css update

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,6 @@ ActiveRecord::Schema.define(version: 20141120171649) do
 
   create_table "skills", force: true do |t|
     t.string   "skill"
-    t.integer  "user_id"
-    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -78,6 +76,7 @@ ActiveRecord::Schema.define(version: 20141120171649) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "summary"
+    t.string   "string"
     t.string   "website"
     t.string   "location"
   end
