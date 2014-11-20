e5a741d389773787507473fe78fcde2a4799d466
PalermoD <palermo.deschamps@gmail.com>
2014-11-20 11:15:08 -0500

    autorization in sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -26,6 +29,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     
     if @user.update_attributes(profile_params)
+       flash[:success] = "Profile updated"
        redirect_to @user
     else
       render 'edit'
@@ -41,7 +45,19 @@ class UsersController < ApplicationController
     end
 
     def profile_params
-    
-      params.require(:user).permit(:name, :email, :website, :location, :summary)
+       params.require(:user).permit(:name, :email, :website, :location, :summary)
+    end
+
+    def logged_in_user
+      unless logged_in?
+      	store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
