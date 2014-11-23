e9a746490d2bf1f51b5067e9c2d8c7297f5d8d91
mike590 <mike.gherard@gmail.com>
2014-11-23 17:23:55 -0500

    When a creator comments on their own project, all the collaborators receive a notification.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,21 @@ class CommentsController < ApplicationController
   def create
     new_comment = Comment.create(comment_params)
     notif_params = notification_params
+    commenter = User.find(notif_params[:user_id])
+    project = Project.find(notif_params[:project_id])
+    if commenter.id == project.user_id
+      # Creator made the comment
+      notif_params[:relation] = "collaborator"
+      project.collabs.each do |collab|
+        temp_params = notif_params
+        notified_user = User.find(collab.user_id)
+        temp_params[:description] = "#{commenter.name} commented on #{project.title}."
+        temp_params[:user_id] = notified_user.id
+        new_comment.notifications.create(temp_params)
+      end
+    else
+      # Collaborator made the comment
+    end
     binding.pry
     redirect_to project_path(params[:comment][:project_id])
   end
